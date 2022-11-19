36119e99e74b61b97ac6032729c9a0e45bb4fa91
sunmoon256 <sunmoon06142@gmail.com>
2022-11-19 16:32:09 +0800

    增加Element UI框架布局

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,46 @@
 <template>
-  <div id="app">
-    <span></span>
-  </div>
+    <div id="app">
+        <el-container class="app-out-panel">
+            <el-header class="sys-header">系统标题</el-header>
+            <el-container class="app-content-panel">
+                <el-aside width="200px" class="sys-menu">左侧菜单</el-aside>
+                <el-main class="sys-map">地图区域</el-main>
+            </el-container>
+        </el-container>
+    </div>
 </template>
 
 <script>
 //import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {}
-}
+    name: 'App',
+    components: {},
+};
 </script>
 
 <style>
-html, body, #app {
-  position: relative;
-  width: 100%;
-  height: 100%;
-  margin: 0px; 
-  /* 设置margin是为了去除Google浏览器自带的8像素外边距 */
+html,
+body,
+#app {
+    position: relative;
+    width: 100%;
+    height: 100%;
+    margin: 0px;
+    /* 设置margin是为了去除Google浏览器自带的8像素外边距 */
+}
+.app-out-panel,
+.app-content-panel {
+    height: 100%;
+}
+.sys-header {
+    background-color: #409eff;
+    line-height: 60px;
+    color: #fff;
+    font-size: 20px;
+    font-weight: 600;
+}
+.sys-menu {
+    background-color: #c0c4cc;
 }
 </style>
