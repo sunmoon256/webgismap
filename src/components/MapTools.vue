c296f5618af068007ec632ebef7c692dc8c029f1
sunmoon256 <sunmoon06142@gmail.com>
2023-03-23 17:42:01 +0800

    优化工具菜单排版

--- src/components/MapTools.vue
+++ src/components/MapTools.vue
@@ -2,8 +2,21 @@
     <div class="maptools-view">
         <span class="maptools-item" @click="handleMapToolsitemClick" id="xzqdh">行政区导航</span>
         <span class="maptools-item" @click="handleMapToolsitemClick" id="maptree">目录树管理</span>
-        <span class="maptools-item" @click="handleMapToolsitemClick" id="distance">距离测量</span>
-        <span class="maptools-item" @click="handleMapToolsitemClick" id="area">面积测量</span>
+        <el-dropdown class="maptools-item" @command="handleCommand">
+            <span class="el-dropdown-link">地图测量<i class="el-icon-arrow-down el-icon--right"></i> </span>
+            <el-dropdown-menu slot="dropdown">
+                <el-dropdown-item icon="el-icon-plus" command="distance">距离测量</el-dropdown-item>
+                <el-dropdown-item icon="el-icon-circle-plus" command="area">面积测量</el-dropdown-item>
+            </el-dropdown-menu>
+        </el-dropdown>
+        <el-dropdown class="maptools-item" @command="handleCommand">
+            <span class="el-dropdown-link">更多功能<i class="el-icon-arrow-down el-icon--right"></i> </span>
+            <el-dropdown-menu slot="dropdown">
+                <el-dropdown-item icon="el-icon-search" command="spacequery">空间查询</el-dropdown-item>
+                <el-dropdown-item icon="el-icon-film" command="morescreen">多屏对比</el-dropdown-item>
+                <el-dropdown-item icon="el-icon-reading" command="swipeana">卷帘分析</el-dropdown-item>
+            </el-dropdown-menu>
+        </el-dropdown>
         <span class="maptools-item" @click="handleMapToolsitemClick" id="clearscreen">清屏</span>
     </div>
 </template>
@@ -22,7 +35,7 @@ export default {
             console.log(currentVisible);
         },
         handleMapToolsitemClick(e) {
-            console.log(e.target.id);
+            // console.log(e.target.id);
             switch (e.target.id) {
                 case 'xzqdh':
                     this.openXZQDHPannel();
@@ -30,11 +43,23 @@ export default {
                 case 'maptree':
                     this.openMapTreePannel();
                     break;
+                case 'clearscreen':
+                    break;
+            }
+        },
+        handleCommand(command) {
+            switch (command) {
                 case 'distance':
                     break;
                 case 'area':
                     break;
-                case 'clearscreen':
+                case 'spacequery':
+                    break;
+                case 'morescreen':
+                    break;
+                case 'swipeana':
+                    break;
+                default:
                     break;
             }
         },
@@ -56,6 +81,16 @@ export default {
     margin-left: 8px;
     margin-right: 8px;
     color: black;
+    font-size: 15px;
     cursor: pointer;
+}
+.maptools-item:hover {
+    color: #409eff;
+}
+.maptools-item:first-child {
+    margin-left: 0px;
+}
+.maptools-item:last-child {
+    margin-right: 0px;
 }
 </style>
