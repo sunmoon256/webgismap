ee3d240143554b7effe9261c79139b8432ac5748
sunmoon256 <sunmoon@henu.edu.cn>
2023-04-24 16:51:10 +0800

    开发用户注册后台接口

--- backend/router/user.js
+++ backend/router/user.js
@@ -4,6 +4,7 @@ var router = express.Router();
 
 var pgConfig = 'postgres://postgres:wsm123@localhost:5432/webgis';
 
+// 获取用户信息的接口
 router.get('/get', function (req, res) {
     var client = new pg.Client(pgConfig);
     client.connect(function (isErr) {
@@ -31,8 +32,36 @@ router.get('/get', function (req, res) {
     });
 });
 
+// 用户注册接口
 router.post('/post', function (req, res) {
-    res.send('user insert');
+    var client = new pg.Client(pgConfig);
+    client.connect(function (isErr) {
+        if (isErr) {
+            console.log('connect error:' + isErr.message);
+            client.end();
+            return;
+        }
+        client.query(
+            'INSERT INTO "user"(name, pwd, phone, email) VALUES ($1,$2,$3,$4);',
+            ['test02', 'webgis', '345678', '345678@qq.com'],
+            function (isErr, rst) {
+                if (isErr) {
+                    console.log('query error:' + isErr.message);
+                    res.send({
+                        status: 'fail',
+                        msg: 'insert error',
+                    });
+                } else {
+                    console.log('query sunccess,data is' + rst);
+                    res.send({
+                        status: 'success',
+                        data: [],
+                    });
+                }
+                client.end();
+            },
+        );
+    });
 });
 
 module.exports = router;
